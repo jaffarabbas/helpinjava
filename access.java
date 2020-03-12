87512de2f7f8fac90c6319a2b74e18289cec4072
jaffarabbas <gamaportal8@gmail.com>
2020-03-13 01:15:19 +0500

    protected access

---
+++ access.java
@@ -0,0 +1,20 @@
+
+ class Main 
+{
+   protected int a;
+
+   Main()
+   {
+           this.a = 434;
+
+           System.out.println(this.a);
+   }
+}
+
+
+class access
+{
+    public static void main(String[] args) {
+        Main obj = new Main();
+    }
+}
