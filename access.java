89bcb9a29a4a19ae15d5eb58bf424c02f0e75539
jaffarabbas <gamaportal8@gmail.com>
2020-03-13 01:38:34 +0500

    Update access.java

--- access.java
+++ access.java
@@ -1,4 +1,34 @@
+import java.lang.reflect.Method;
 
+class privat
+ {
+     private int a;
+
+     void geta(int a)
+     {
+         this.a = a;
+     }
+
+     int seta()
+     {
+         return this.a;
+     }
+
+     privat()
+     {
+         this.a=234234;
+     }
+
+     private void mon()
+     {
+         System.out.println(this.a);
+     }
+
+ }
+ 
+ 
+ 
+ 
  class Main //proctedd
 {
    protected int a;
@@ -14,7 +44,16 @@
 
 class access
 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         Main obj = new Main();
+        privat check = new privat();//invoking method
+
+        System.out.println("Private vaiable with a private method");
+
+        Method m = privat.class.getDeclaredMethod("mon");
+
+        m.setAccessible(true);
+
+        m.invoke(check);
     }
 }
