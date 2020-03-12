6ddcb3e8f265b937db221089153e2eb93c804eac
jaffarabbas <gamaportal8@gmail.com>
2020-03-13 00:29:58 +0500

    checking private method

---
+++ first.java
@@ -0,0 +1,22 @@
+
+import java.lang.reflect.Method;
+import java.util.Random;
+import java.lang.reflect.Method; 
+
+class Main
+{
+    private String var1;
+
+    private void name() {
+        int a = 4;
+        System.out.println(a);
+    }
+}
+
+
+class first
+{
+    public static void main(String[] args) throws Exception  {
+       
+    }
+}
