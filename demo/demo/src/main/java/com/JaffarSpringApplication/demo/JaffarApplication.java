bb4c8f95bb842a14010c0f6c099c1f11c3fdcd96
jaffarabbas <gamaportal8@gmail.com>
2021-02-07 18:50:49 +0500

    2-7-2021    Spring Application inserted

---
+++ demo/demo/src/main/java/com/JaffarSpringApplication/demo/JaffarApplication.java
@@ -0,0 +1,13 @@
+package com.JaffarSpringApplication.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JaffarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JaffarApplication.class, args);
+	}
+
+}
---
+++ demo/demo/src/test/java/com/JaffarSpringApplication/demo/JaffarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JaffarSpringApplication.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaffarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
