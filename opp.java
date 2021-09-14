fdb0c8b08a25f8a045c27ec3736b1ae6262fa4e8
jaffarabbas <gamaportal8@gmail.com>
2021-09-14 23:03:47 +0500

    9-14-2021

--- opp.java
+++ opp.java
@@ -1,8 +1,19 @@
-class opp{
-	public static void main(String[] args) {
-		int a = 23423;
-		System.out.println("Helllo wolrd"+a);
-	}
-}
+#from number 
 
+def fun1(a):
+    if a < 0:
+        return a
+    return -1 * a
 
+# from using concatination
+def func(a):
+    value = "-" + str(a)
+    if value[0] == "-":
+        return a
+    return int(value)
+    
+print(fun1(-1))
+
+print(func(-1))
+
+print(type(func(-1)))
