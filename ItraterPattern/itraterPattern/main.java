1fce6317e8ce0658df5339eb8436f243725431b0
Jaffar Abbas <gamaportal8@gmail.com>
2022-08-03 15:33:12 +0500

    Commited File : main.java at 8/3/2022 3:33:12 PM

--- ItraterPattern/itraterPattern/main.java
+++ ItraterPattern/itraterPattern/main.java
@@ -1,2 +1,13 @@
-package PACKAGE_NAME;public class main {
+import java.util.ArrayList;
+
+public class main {
+    public static void main(String[] args) {
+        ArrayList<Employee> employees = new ArrayList<>();
+        employees.add(new Employee(1,"jaffar"));
+        employees.add(new Employee(2,"ahmed"));
+
+        for (var item : employees) {
+            System.out.println(item.id + "\n" + item.name);
+        }
+    }
 }
